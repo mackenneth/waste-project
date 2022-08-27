7c968487189e0099d122968885fa05989f307918
Maksim <mackenneth@yandex.ru>
2022-08-27 19:37:37 +0300

    refactor: changed class names

--- src/js/consts/elements.ts
+++ src/js/consts/elements.ts
@@ -1,4 +1,4 @@
-export const timerInner = document.querySelector( '.countdown__time-value' )
+export const timerInner = document.querySelector( '.game-data__time-value' )
 export const countdown = document.querySelector('.countdown')
 export const countdownCircle = document.querySelector('.countdown__circle')
 export const wasteContainerTemplate = document.querySelector('#waste-container-template')
